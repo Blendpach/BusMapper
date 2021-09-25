5866f40d7a712fa971dd7df55b3dbc7e4608f76f
viduviduranga <vidushabamunugama@gmail.com>
2021-09-25 20:52:28 +0530

    deleted unwanted things
    deleted unwanted things

--- src/main/java/com/blendpach/busmapper/LocationController.java
+++ src/main/java/com/blendpach/busmapper/LocationController.java
@@ -15,39 +15,8 @@ import org.json.JSONArray;
 
 
 @RestController
-public class StudentController {
-
-    @GetMapping("/student")
-    public Student getStudent() {
-
-        return new Student("Ushira", "Tissera");
-
-    }
-
-    @GetMapping("/students")
-    public List<Student> getstudents() {
-
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Dinith", "Shavindu"));
-        students.add(new Student("Name1", "Surname1"));
-        students.add(new Student("Name2", "Surname2"));
-        students.add(new Student("Name3", "Surname3"));
-        students.add(new Student("Name4", "Surname4"));
-        students.add(new Student("Name5", "Surname5"));
-        students.add(new Student("Name6", "Surname6"));
-
-        return students;
-
-    }
-
-    @GetMapping("/student/{firstName}/{lastName}")
-    public Student studentPathVariable(@PathVariable("firstName") String firstName,
-            @PathVariable("lastName") String lastName) {
-
-        return new Student(firstName, lastName);
-
-    }
-
+public class LocationController {
+   
     @GetMapping("/directions/{startLat}/{startLng}/{endLat}/{endLng}")
     public List<location> getDirectios(@PathVariable("startLat") String startLat,@PathVariable("startLng") String startLng,@PathVariable("endLat") String endLat,@PathVariable("endLng") String endLng) throws Exception {
         String url = "https://maps.googleapis.com/maps/api/directions/json?origin="+startLat+","+startLng+"&destination="+endLat+","+endLng+"&transit_mode=bus&mode=transit&key=AIzaSyDP3V4_sogsaHcONLPS9d59Ccq_IQhDygQ";
